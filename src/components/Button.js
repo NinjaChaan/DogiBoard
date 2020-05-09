eb97b843ccfc5f7131df8651e870afef421c2348
Ninya <sleepyneko@protonmail.ch>
2020-05-09 15:18:19 +0300

    Ismobile to change warning button to mobile friendly one

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,7 @@
 /* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable indent */
 import styled, { css } from 'styled-components'
+import { isMobile } from 'react-device-detect'
 import { device } from '../devices'
 
 // eslint-disable-next-line import/prefer-default-export
@@ -21,7 +22,8 @@ const Button = styled.button`
 	background-color: ${(props) =>
 		(props.primary && props.theme.colors.primary.backgroundColor)
 		|| (props.warning && props.theme.colors.warning.backgroundColor)
-		|| (props.warning_light && props.theme.colors.warning_light.backgroundColor)
+		|| (props.warning_light && !isMobile && props.theme.colors.warning_light.backgroundColor)
+		|| (props.warning_light && isMobile && props.theme.colors.warning.backgroundColor)
 		|| (props.light && props.theme.colors.light.backgroundColor)
 		|| (props.link && props.theme.colors.link.backgroundColor)
 		|| props.theme.colors.primary.backgroundColor
@@ -29,26 +31,19 @@ const Button = styled.button`
 	color: ${(props) =>
 		(props.primary && props.theme.colors.primary.color)
 		|| (props.warning && props.theme.colors.warning.color)
-		|| (props.warning_light && props.theme.colors.warning_light.color)
+		|| (props.warning_light && !isMobile && props.theme.colors.warning_light.color)
+		|| (props.warning_light && isMobile && props.theme.colors.warning.color)
 		|| (props.light && props.theme.colors.light.color)
 		|| (props.link && props.theme.colors.link.color)
 		|| props.theme.colors.primary.color
 	};
-
-	@media ${device.tabletMAX} {
-		background-color: ${(props) =>
-		(props.warning_light && props.theme.colors.warning.backgroundColor)
-	};
-		color: ${(props) =>
-		(props.warning_light && props.theme.colors.warning.color)
-	};
-	}
 	
 	&:hover {
 	background-color: ${(props) =>
 		(props.primary && props.theme.colors.primary.hover.backgroundColor)
 		|| (props.warning && props.theme.colors.warning.hover.backgroundColor)
-		|| (props.warning_light && props.theme.colors.warning_light.hover.backgroundColor)
+		|| (props.warning_light && !isMobile && props.theme.colors.warning_light.hover.backgroundColor)
+		|| (props.warning_light && isMobile && props.theme.colors.warning.hover.backgroundColor)
 		|| (props.light && props.theme.colors.light.hover.backgroundColor)
 		|| (props.link && props.theme.colors.link.hover.backgroundColor)
 		|| (props.theme.colors.primary.hover.backgroundColor)
@@ -57,7 +52,8 @@ const Button = styled.button`
 	color: ${(props) =>
 		(props.primary && props.theme.colors.primary.hover.color)
 		|| (props.warning && props.theme.colors.warning.hover.color)
-		|| (props.warning_light && props.theme.colors.warning_light.hover.color)
+		|| (props.warning_light && !isMobile && props.theme.colors.warning_light.hover.color)
+		|| (props.warning_light && isMobile && props.theme.colors.warning.hover.color)
 		|| (props.light && props.theme.colors.light.hover.color)
 		|| (props.link && props.theme.colors.link.hover.color)
 		|| (props.theme.colors.primary.hover.color)
@@ -74,7 +70,8 @@ const Button = styled.button`
 	background-color: ${(props) =>
 		(props.primary && props.theme.colors.primary.focus.backgroundColor)
 		|| (props.warning && props.theme.colors.warning.focus.backgroundColor)
-		|| (props.warning_light && props.theme.colors.warning_light.focus.backgroundColor)
+		|| (props.warning_light && !isMobile && props.theme.colors.warning_light.focus.backgroundColor)
+		|| (props.warning_light && isMobile && props.theme.colors.warning.focus.backgroundColor)
 		|| (props.light && props.theme.colors.light.focus.backgroundColor)
 		|| (props.link && props.theme.colors.link.focus.backgroundColor)
 		|| (props.theme.colors.primary.focus.backgroundColor)
@@ -83,7 +80,8 @@ const Button = styled.button`
 	color: ${(props) =>
 		(props.primary && props.theme.colors.primary.focus.color)
 		|| (props.warning && props.theme.colors.warning.focus.color)
-		|| (props.warning_light && props.theme.colors.warning_light.focus.color)
+		|| (props.warning_light && !isMobile && props.theme.colors.warning_light.focus.color)
+		|| (props.warning_light && isMobile && props.theme.colors.warning.focus.color)
 		|| (props.light && props.theme.colors.light.focus.color)
 		|| (props.link && props.theme.colors.link.focus.color)
 		|| (props.theme.colors.primary.focus.color)
